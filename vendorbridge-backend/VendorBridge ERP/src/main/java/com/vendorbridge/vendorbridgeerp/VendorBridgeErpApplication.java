091c0b79ad03151f89583731f5578ff495bcd210
omsolanki271 <omsolanki271@gmail.com>
2026-06-06 09:38:38 +0530

    Initial project setup

---
+++ vendorbridge-backend/VendorBridge ERP/src/main/java/com/vendorbridge/vendorbridgeerp/VendorBridgeErpApplication.java
@@ -0,0 +1,13 @@
+package com.vendorbridge.vendorbridgeerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorBridgeErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendorBridgeErpApplication.class, args);
+    }
+
+}
---
+++ vendorbridge-backend/VendorBridge ERP/src/test/java/com/vendorbridge/vendorbridgeerp/VendorBridgeErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendorbridge.vendorbridgeerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorBridgeErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
